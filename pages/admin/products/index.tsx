f1f38406322ab9923e1ed63528ecdd5dfda4f9f5
mb <minhcf2013@gmail.com>
2021-03-08 15:26:02 +0900

    clean the junks

--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -1,39 +1,9 @@
-import {
-  Box,
-  Button,
-  Chip,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Grid,
-  InputLabel,
-  makeStyles,
-  Slide,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Tooltip,
-  Typography,
-} from "@material-ui/core";
-import Pagination from "@material-ui/lab/Pagination";
-
-import { blue, green, grey, pink, red } from "@material-ui/core/colors";
+import { Box, Button, Grid, makeStyles } from "@material-ui/core";
+import { blue, green, grey, red } from "@material-ui/core/colors";
 import React from "react";
 import AddCircleTwoToneIcon from "@material-ui/icons/AddCircleTwoTone";
-import EditTwoToneIcon from "@material-ui/icons/EditTwoTone";
-import DeleteTwoToneIcon from "@material-ui/icons/DeleteTwoTone";
-import WarningTwoToneIcon from "@material-ui/icons/WarningTwoTone";
-import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import { SectionTitle } from "../../../components/typography/section-title";
-import { TransitionProps } from "@material-ui/core/transitions/transition";
 import { Page } from "../../../components/page";
-import { Field, Form, Formik } from "formik";
-import { OutlinedTextfield } from "../../../components/form/outlined-textfield";
 import { ProductList } from "../../../components/admin/products/product-list";
 import { OptionList } from "../../../components/admin/products/option-list";
 import { FlavorList } from "../../../components/admin/products/flavor-list";
@@ -151,24 +121,8 @@ const useStyles = makeStyles({
   tableHead: {},
 });
 
-const Transition = React.forwardRef(function Transition(
-  props: TransitionProps & { children?: React.ReactElement<any, any> },
-  ref: React.Ref<unknown>,
-) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
-
 const UsersPage: React.FC = () => {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
   return (
     <Page title={"レジ"}>
       <Grid container>
@@ -220,63 +174,6 @@ const UsersPage: React.FC = () => {
             <FlavorList />
           </Grid>
         </Box>
-        <Dialog
-          open={open}
-          TransitionComponent={Transition}
-          keepMounted
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-slide-title"
-          aria-describedby="alert-dialog-slide-description"
-        >
-          <DialogTitle id="alert-dialog-slide-title">本当に削除しますか？</DialogTitle>
-          <Button onClick={handleClose} color="secondary">
-            削除
-          </Button>
-        </Dialog>
-        {/* <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title">発注</DialogTitle>
-          <DialogContent>
-            <DialogContentText>値段: 230円</DialogContentText>
-            <Box display="flex" flexDirection="row" alignItems="center">
-              <Box width={1} display="flex" alignItems="center">
-                <Box maxWidth={60} bgcolor={pink[100]} borderRadius={10}>
-                  <img className={classes.img} src={"/images/4.png"} />
-                </Box>
-                <Typography className={classes.name}>コーン</Typography>
-              </Box>
-              <Box>
-                <Formik
-                  initialValues={{ quantity: 0 }}
-                  onSubmit={async (values, { setSubmitting }) => {
-                    console.log("something");
-                    setSubmitting(true);
-                  }}
-                >
-                  {() => (
-                    <Form>
-                      <Box width={1}>
-                        <Box width={1}>
-                          <Field component={OutlinedTextfield} name="quantity" />
-                        </Box>
-                      </Box>
-                    </Form>
-                  )}
-                </Formik>
-              </Box>
-              <Box width={1} textAlign="center">
-                <Typography className={classes.orderPricing}>6000円</Typography>
-              </Box>
-            </Box>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">
-              キャンセル
-            </Button>
-            <Button onClick={handleClose} color="primary">
-              発注
-            </Button>
-          </DialogActions>
-        </Dialog> */}
       </Grid>
     </Page>
   );
